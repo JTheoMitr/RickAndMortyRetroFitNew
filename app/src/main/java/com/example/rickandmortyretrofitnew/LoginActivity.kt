9f589f202030c5f26e7625482a1f2b779d2eda55
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2022-06-10 13:49:02 -0500

    updated login screen with viewBinding and a cleanly cropped background

--- app/src/main/java/com/example/rickandmortyretrofitnew/LoginActivity.kt
+++ app/src/main/java/com/example/rickandmortyretrofitnew/LoginActivity.kt
@@ -2,11 +2,17 @@ package com.example.rickandmortyretrofitnew
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.rickandmortyretrofitnew.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private var _binding: ActivityLoginBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        _binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         supportActionBar?.hide()
     }
