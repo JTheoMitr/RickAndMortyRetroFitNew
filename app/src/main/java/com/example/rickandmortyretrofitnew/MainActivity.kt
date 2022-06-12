c8fa503db42a1a95c2b2d414e45ed2d92f23522c
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2022-06-11 19:37:18 -0500

    now using navigation component with fragments

--- app/src/main/java/com/example/rickandmortyretrofitnew/MainActivity.kt
+++ app/src/main/java/com/example/rickandmortyretrofitnew/MainActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.rickandmortyretrofitnew.databinding.ActivityMainBinding
@@ -13,54 +16,17 @@ import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
-    private var _binding: ActivityMainBinding? = null
-    private val binding get() = _binding!!
-
-    private val viewModel : MainViewModel by lazy {
-        ViewModelProvider(this).get(MainViewModel::class.java)
-    }
-
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        _binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
-        viewModel.characterLiveData.observe(this) { state ->
-            processCharacterResponse(state)
-
-        }
-
+        navController = Navigation.findNavController(this, R.id.fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
     }
 
-    private fun processCharacterResponse(state: ScreenState<List<Character>?>) {
-
-        val pb = binding.progressBar
-
-        when(state) {
-            is ScreenState.Loading -> {
-                pb.visibility = View.VISIBLE
-            }
-            is ScreenState.Success -> {
-                pb.visibility = View.GONE
-
-                if(state.data != null) {
-
-                    val adapter = MainAdapter(state.data)
-                    val recyclerView = findViewById<RecyclerView>(R.id.charactersRv)
-
-                    recyclerView?.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
-                    recyclerView?.adapter = adapter
-
-                }
-            }
-
-            is ScreenState.Error -> {
-                pb.visibility = View.GONE
-                val view = pb.rootView
-                Snackbar.make(view, state.message!!, Snackbar.LENGTH_LONG).show()
-
-            }
-        }
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
     }
 }
