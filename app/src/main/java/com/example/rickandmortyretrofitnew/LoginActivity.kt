b4de3fc78b18efa725cc329b7cdab21653097c54
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2022-06-13 18:32:07 -0500

    added new dependencies for google SSO and updated login activity

--- app/src/main/java/com/example/rickandmortyretrofitnew/LoginActivity.kt
+++ app/src/main/java/com/example/rickandmortyretrofitnew/LoginActivity.kt
@@ -4,8 +4,18 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.rickandmortyretrofitnew.databinding.ActivityLoginBinding
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
+
+    companion object {
+        const val CONST_SIGN_IN = 34
+    }
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var googleAuth: GoogleSignInClient
 
     private var _binding: ActivityLoginBinding? = null
     private val binding get() = _binding!!
@@ -15,12 +25,20 @@ class LoginActivity : AppCompatActivity() {
         _binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        auth = FirebaseAuth.getInstance()
+
         supportActionBar?.hide()
 
         binding.loginBtn.setOnClickListener {
 
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+            googleSignIn()
+
+//            val intent = Intent(this, MainActivity::class.java)
+//            startActivity(intent)
         }
+    }
+
+    private fun googleSignIn() {
+        TODO("Not yet implemented")
     }
 }
