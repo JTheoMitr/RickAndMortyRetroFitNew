f0c22560d6c338311e0dea602144cfe80e5bbee3
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2022-06-14 14:26:37 -0500

    Google Sign-In now working, need to verify with sign out testing and streamlining code

--- app/src/main/java/com/example/rickandmortyretrofitnew/LoginActivity.kt
+++ app/src/main/java/com/example/rickandmortyretrofitnew/LoginActivity.kt
@@ -3,9 +3,15 @@ package com.example.rickandmortyretrofitnew
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.rickandmortyretrofitnew.databinding.ActivityLoginBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
@@ -33,12 +39,60 @@ class LoginActivity : AppCompatActivity() {
 
             googleSignIn()
 
-//            val intent = Intent(this, MainActivity::class.java)
-//            startActivity(intent)
+//
         }
+
+        // Configure google sign in
+        val gso = GoogleSignInOptions
+            .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.client_id))
+            .requestEmail()
+            .build()
+
+        googleAuth = GoogleSignIn.getClient(this, gso)
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String?) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's info
+                    Log.d("SSO", "signInWithCredential:success")
+                    val user = auth.currentUser
+                    startActivity(Intent(this, MainActivity::class.java))
+                } else {
+                    // If sign-in fails, display a message to the user
+                    Log.w("SSO", "signInWithCredential:failure", task.exception)
+                    //updateUI(null)
+                }
+
+            }
     }
 
     private fun googleSignIn() {
-        TODO("Not yet implemented")
+
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+        if (account == null) {
+            val signInIntent = googleAuth.signInIntent
+            startActivityForResult(signInIntent, CONST_SIGN_IN)
+        } else if (account != null) {
+            firebaseAuthWithGoogle(account.idToken)
+        }
     }
+
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//        if (requestCode == CONST_SIGN_IN) {
+//            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+//            try {
+//                val account = task.getResult(ApiException::class.java)
+//                firebaseAuthWithGoogle(account.idToken)
+//            }
+//            catch (e:ApiException) {
+//                Toast.makeText(this, "$e", Toast.LENGTH_LONG).show()
+//            }
+//        }
+//    }
 }
+
--- app/src/main/java/com/example/rickandmortyretrofitnew/MainActivity.kt
+++ app/src/main/java/com/example/rickandmortyretrofitnew/MainActivity.kt
@@ -5,15 +5,20 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
+import com.google.firebase.auth.FirebaseAuth
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
+    private lateinit var auth: FirebaseAuth
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        auth = FirebaseAuth.getInstance()
+
 
         navController = Navigation.findNavController(this, R.id.fragment)
         NavigationUI.setupActionBarWithNavController(this, navController)
